6d9b72471134917e81bc32f158b4bade2f80e6f0
daniel mbuthia <danielwandurua@gmail.com>
2022-07-06 22:13:41 +0000

    add docString

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def is_leap(year):
+  """ Takes an year and returs if the year is leap year or not """
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
@@ -11,6 +12,7 @@ def is_leap(year):
     return False
 
 def days_in_month(year,month):
+    """Takes an year and a month and returns the number of days for that month and year"""
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if month == 2:
         leap_year = is_leap(year)
